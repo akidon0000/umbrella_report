be0c9aa013a78cdd945fe3258381b4fdf90aa238
Akihiro Matsuyama <a@art.jp>
2021-09-27 11:31:49 +0900

    LineToken 変更

--- report/docker/src/main.py
+++ report/docker/src/main.py
@@ -4,10 +4,6 @@ import datetime
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-# import logging
-# from multiprocessing import Pool
-# from scheduler.job import JobController
-# import schedule
 import time
 
 from linebot import (
@@ -37,35 +33,19 @@ line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 
 def main():
   # line_bot_api.broadcast(TextSendMessage(text="メンテナンス"))
-  # 10分ごと
-  # schedule.every(10).minutes.do(report)
-
-  # 2時間ごと
-  # schedule.every(2).hours.do(report)
-
-  # 毎日0時1分
-  # schedule.every().day.at("00:01").do(report)
-
-  # 毎週月曜日
-  # schedule.every().monday.do(report)
-
-  # while True:
-  #     schedule.run_pending()
-  #     time.sleep(30)
   report()
-
 
 
 def report():
 
   dt_now = datetime.datetime.now()
 
-
-  log_file_name = str(datetime.date(dt_now.year, dt_now.month, dt_now.day-1)) + ".log"
+  time = str(datetime.date(dt_now.year, dt_now.month, dt_now.day-1))
+  log_file_name = time + ".log"
   print(dt_now)
   print(log_file_name)
-  line_bot_api.broadcast(TextSendMessage(text=str(dt_now)))
-  line_bot_api.broadcast(TextSendMessage(text=log_file_name))
+  textMessage = ""
+  textMessage += "===" + time + "===\n"
 
   errorPath = "/python/src/logFiles/ErrorLogFiles/" + log_file_name
   successPath = "/python/src/logFiles/SuccessLogFiles/" + log_file_name
@@ -79,24 +59,24 @@ def report():
     f.close()
 
     successCount = data.count("[Success]")
-    line_bot_api.broadcast(TextSendMessage(text="==本日の稼働状況==\n" + "通信回数：" + str(successCount)))
+    textMessage += "通信回数：" + str(successCount) + "\n"
 
   else:
-    line_bot_api.broadcast(TextSendMessage(text="==本日の稼働状況==\n" + "通信回数：" + "0"))
+    textMessage += "通信回数：0\n"
 
 
   # ErrorPath
   if os.path.exists(errorPath):
-    line_bot_api.broadcast(TextSendMessage(text="サーバーにERRORが発生しています"))
-
+    textMessage += "サーバー:ERROR\n"
     f = open(path, 'r')
     data = f.read()
     f.close()
-    line_bot_api.broadcast(TextSendMessage(text="==ERROR内容==\n" + data))
+    textMessage += "==ERROR内容==\n" + data
 
   else:
-    line_bot_api.broadcast(TextSendMessage(text="サーバーは正常に稼働中"))
+    textMessage += "サーバー:正常"
 
+  line_bot_api.broadcast(TextSendMessage(text=textMessage))
 
 if __name__ == "__main__":
     main()
